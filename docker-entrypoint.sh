38593f355f1911a869aaf646800c4b31c148a4fa
aisuhua <1079087531@qq.com>
2024-03-15 08:38:26 +0800

    add sync base yum

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,10 +11,12 @@ for str in ${myArray[@]}; do
   yum clean all
   yum makecache
   reposync -n --norepopath --repoid=ks10-adv-updates --arch=x86_64 -p "/mirror/os/kylin/${str}/updates/x86_64"
+  reposync -n --norepopath --repoid=ks10-adv-os --arch=x86_64 -p "/mirror/os/kylin/${str}/base/x86_64"
   
   rm -rf /etc/yum.repos.d/kylin.repo
   cp "/src/repo/${str}_aarch64.repo" /etc/yum.repos.d/kylin.repo
   yum clean all
   yum makecache
   reposync -n --norepopath --repoid=ks10-adv-updates --arch=aarch64 -p "/mirror/os/kylin/${str}/updates/aarch64"
+  reposync -n --norepopath --repoid=ks10-adv-os --arch=aarch64 -p "/mirror/os/kylin/${str}/base/aarch64"
 done
